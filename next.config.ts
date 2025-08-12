a0be802069b788499e2c3ab409e789d85a4b45c3
Mark Josh Sabeniano <markjosh112345@gmail.com>
2025-08-12 19:33:26 +0000

    why is it that all of the pics uploaded here was corrupted. can you help

--- next.config.ts
+++ next.config.ts
@@ -8,16 +8,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
 };
 
 export default nextConfig;

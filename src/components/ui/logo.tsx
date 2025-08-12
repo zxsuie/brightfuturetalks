6bed74b4f0d84888294e6bdbb611bea59fcfa64b
Mark Josh Sabeniano <markjosh112345@gmail.com>
2025-08-12 12:35:34 +0000

    why is it that i can't show pics on this website? is there something wro

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,9 +1,10 @@
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
 
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn('flex items-center gap-2', className)}>
-      <img src="/photos/BFTLOGO.png" alt="Bright Future Talks Logo" className="h-6 w-6" />
+      <Image src="/photos/BFTLOGO.png" alt="Bright Future Talks Logo" width={24} height={24} className="h-6 w-6" />
       <span className="text-lg font-headline font-bold">
         Bright Future Talks
       </span>

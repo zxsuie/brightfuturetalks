9323c524a71b1dabe2924a20f45b31cbb184ab2e
Mark Josh Sabeniano <markjosh112345@gmail.com>
2025-08-12 21:04:41 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -8,7 +8,7 @@ const TiktokIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
-    fill="currentColor"
+    fill="none"
     stroke="currentColor"
     strokeWidth="2"
     strokeLinecap="round"

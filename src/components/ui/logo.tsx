c890042b2a18c31055727cf87c13678dc61048e8
Mark Josh Sabeniano <markjosh112345@gmail.com>
2025-08-12 12:32:04 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -3,7 +3,7 @@ import { cn } from '@/lib/utils';
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn('flex items-center gap-2', className)}>
-      <img src="/photo/BFTLOGO.svg" alt="Bright Future Talks Logo" className="h-6 w-6" />
+      <img src="/photos/BFTLOGO.png" alt="Bright Future Talks Logo" className="h-6 w-6" />
       <span className="text-lg font-headline font-bold">
         Bright Future Talks
       </span>

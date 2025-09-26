db08915694b519689e3515fecce05eef404297c3
Mark Josh Sabeniano <markjosh112345@gmail.com>
2025-09-26 08:33:06 +0000

    so, i'm using klaviyo for our email marketing and i enabled a website tr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
@@ -40,6 +41,21 @@ export default function RootLayout({
           'min-h-screen bg-background font-body antialiased'
         )}
       >
+        <Script
+          async
+          type="text/javascript"
+          src="https://static.klaviyo.com/onsite/js/UgxRfS/klaviyo.js?company_id=UgxRfS"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="klaviyo-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(){if(!window.klaviyo){window._klOnsite=window._klOnsite||[];try{window.klaviyo=new Proxy({},{get:function(n,i){return"push"===i?function(){var n;(n=window._klOnsite).push.apply(n,arguments)}:function(){for(var n=arguments.length,o=new Array(n),w=0;w<n;w++)o[w]=arguments[w];var t="function"==typeof o[o.length-1]?o.pop():void 0,e=new Promise((function(n){window._klOnsite.push([i].concat(o,[function(i){t&&t(i),n(i)}]))}));return e}}})}catch(n){window.klaviyo=window.klaviyo||[],window.klaviyo.push=function(){var n;(n=window._klOnsite).push.apply(n,arguments)}}}}();
+            `,
+          }}
+        />
         <Header />
         <main>{children}</main>
         <Footer />

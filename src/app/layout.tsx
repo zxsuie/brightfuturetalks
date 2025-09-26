3cb920c3142df01ad8bc54fb0cd1bac9003ef43b
Mark Josh Sabeniano <markjosh112345@gmail.com>
2025-09-26 13:07:04 +0000

    why is it that the klaviyo onsite code is still not working? it's still

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,11 +39,11 @@ export default function RootLayout({
           async
           type="text/javascript"
           src="https://static.klaviyo.com/onsite/js/UgxRfS/klaviyo.js?company_id=UgxRfS"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
         <Script
           id="klaviyo-init"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               !function(){if(!window.klaviyo){window._klOnsite=window._klOnsite||[];try{window.klaviyo=new Proxy({},{get:function(n,i){return"push"===i?function(){var n;(n=window._klOnsite).push.apply(n,arguments)}:function(){for(var n=arguments.length,o=new Array(n),w=0;w<n;w++)o[w]=arguments[w];var t="function"==typeof o[o.length-1]?o.pop():void 0,e=new Promise((function(n){window._klOnsite.push([i].concat(o,[function(i){t&&t(i),n(i)}]))}));return e}}})}catch(n){window.klaviyo=window.klaviyo||[],window.klaviyo.push=function(){var n;(n=window._klOnsite).push.apply(n,arguments)}}}}();
